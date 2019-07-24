64d731872bb3641ff20b35019fc4d7e893423456
komamiku <mikuma1503@gmail.com>
2019-07-25 02:31:58 +0900

    Modify for repeating setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 # link
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.vim ~/.vim
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/.tmux.d ~/.tmux.d
+ln -snf ~/dotfiles/.vimrc ~/.vimrc
+ln -snf ~/dotfiles/.vim ~/.vim
+ln -snf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -snf ~/dotfiles/.tmux.d ~/.tmux.d
 
 # vim plugin setup
+rm -rf ~/dotfiles/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/dotfiles/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
